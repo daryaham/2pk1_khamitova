2b129c1acc0fb8a368d12ead9acab0c5e679c587
Darya <Darya@DESKTOP-DK96K9O>
2023-09-27 16:27:54 +0500

    Добавьте файлы проекта.

---
+++ PZ_1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace PZ_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0;
+            double b = 4;
+            double c = 1;
+            double result;
+
+            result = 1.5 * Math.Pow(a - b, 2) / (Math.Abs(a - b) * c) + Math.Pow(10, 4) * Math.Sqrt(Math.Abs(a - b)) - 2.5 * (Math.Pow(a, 2) + 2.75) * Math.Sin(-2 * a) / 3 + Math.Pow(a, 2) * b * c;
+
+            Console.WriteLine(result);
+        }
+    }
+}
