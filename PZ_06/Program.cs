f06403bf1d1f2eb90dd0452d83d8e06f1dccd02d
Хамитова Дарья <khamitova.milena@list.ru>
2023-09-29 17:11:49 +0500

    Добавлена шестая практика

---
+++ PZ_06/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PZ_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] massive = new double[20];
+            for (int i = 0; i < 20; i++)
+            {
+                massive[i] = Math.Pow(2, i) / 2;
+            }
+            Array.Sort(massive);
+            Array.Reverse(massive);
+            Console.Write("\nМассив в порядке уменьшения: ");
+            foreach(double j in massive) 
+            {
+                Console.Write($"{j} ");
+            }
+        }
+    }
+}
