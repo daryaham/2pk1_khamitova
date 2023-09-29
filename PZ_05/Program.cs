445e37c2e43df8e282ec95ad3d0981a864750cdb
Хамитова Дарья <khamitova.milena@list.ru>
2023-09-29 17:01:07 +0500

    Добавлена пятая практика

---
+++ PZ_05/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PZ_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите начальную температуру в градусах Цельсия:");
+            double startCelsius = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите конечную температуру в градусах Цельсия:");
+            double endCelsius = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите шаг изменения температуры в градусах Цельсия:");
+            double step = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Температура (°C)\tТемпература (°F)");
+
+            for (double celsius = startCelsius; celsius <= endCelsius; celsius += step)
+            {
+                double fahrenheit = (9.0 / 5.0) * celsius + 32;
+                Console.WriteLine($"{celsius}\t\t{fahrenheit}");
+            }
+        }
+    }
+}
